d4661b8e554cb146c2cebdbf160aed951ac4f25c
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-07-23 20:55:07 +0530

    pizza created

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -11,7 +11,16 @@ function App() {
 }
 
 function Pizza() {
-  return <h2>Pizza</h2>;
+  return (
+    <div>
+      <img
+        src="https://ik.imagekit.io/suhailakhtar039/pizza.jpg?updatedAt=1753284012004"
+        alt="It's a pizza"
+      />
+      <h2>Pizza Spinachi</h2>
+      <p>Onion, spinach, oregano, and cheese</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
