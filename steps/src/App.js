21ad6284d64ec3d3ad93c9adfa0ef01b4097bf05
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-07-27 15:41:10 +0530

    content filled

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,22 @@
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">Hello</p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
