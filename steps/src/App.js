dd63d4ba02b0a078a9262defc859fd05f2829bc0
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-07-27 21:38:39 +0530

    onclick function defined reactive way

--- steps/src/App.js
+++ steps/src/App.js
@@ -15,7 +15,12 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+          onClick={() => {
+            alert("Previous clicked");
+          }}
+        >
           Previous
         </button>
         <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
